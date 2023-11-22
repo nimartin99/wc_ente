0ed51f89968f19f1b83a464da78b8de407365da2
nimartin99 <nic0m4rtin@gmail.com>
2023-11-22 13:04:05 +0100

    Merged with Jakob for current version 0.1 of the game. Game now has feelgood movement, random obstacles with detection, powerups, tunnel shader, UI with key configuration

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -8,6 +8,13 @@ public class GameInitializer : MonoBehaviour
     [SerializeField] private Transform playerPrefab;
     [SerializeField] private Transform powerUpPrefab;
     
+    //set standard keycodes for first two players
+    private KeyCode[,] standardCodes =
+    {
+        { KeyCode.W,KeyCode.A,KeyCode.S,KeyCode.D},
+        { KeyCode.UpArrow,KeyCode.LeftArrow,KeyCode.DownArrow,KeyCode.RightArrow}
+    };
+    
     private void Awake() {
         if (Instance != null && Instance != this) { 
             Destroy(this); 
@@ -23,10 +30,11 @@ public class GameInitializer : MonoBehaviour
             playerAnchor.position = new Vector3(0, 0, 4f);
             playerAnchor.eulerAngles = new Vector3(0, 0, 90 * i);
             DuckControls playerScript = playerAnchor.GetChild(0).GetComponent<DuckControls>();
-            playerScript.keyUp = uiControl.players[i].playerUp;
-            playerScript.keyLeft = uiControl.players[i].playerLeft;
-            playerScript.keyDuck = uiControl.players[i].playerDuck;
-            playerScript.keyRight = uiControl.players[i].playerRight;
+            Debug.Log(uiControl.players[i].playerUp);
+            playerScript.keyUp = uiControl.players[i].playerUp != KeyCode.None ? uiControl.players[i].playerUp: standardCodes[i,0];
+            playerScript.keyLeft = uiControl.players[i].playerLeft != KeyCode.None ? uiControl.players[i].playerLeft: standardCodes[i,1];;
+            playerScript.keyDuck = uiControl.players[i].playerDuck != KeyCode.None ? uiControl.players[i].playerDuck: standardCodes[i,2];;
+            playerScript.keyRight = uiControl.players[i].playerRight != KeyCode.None ? uiControl.players[i].playerRight: standardCodes[i,3];;
         }
         Instantiate(powerUpPrefab, new Vector3(0, -0.4f, 0), Quaternion.identity);
     }
